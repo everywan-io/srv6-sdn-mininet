820b9a56a196e622908ba929d5531144ccf74958
bogdaniatco <bogdan.iatco@hotmail.it>
2020-01-27 10:57:34 +0100

    merge conflict

--- scripts/port_forwarding.sh
+++ scripts/port_forwarding.sh
@@ -20,10 +20,13 @@ INPUT_INTERFACE=${INTERFACES[$INPUT_INTERFACE]}
 # IP of the interface facing the public net
 IP_INTERFACE=${IPS[$INPUT_INTERFACE]}
 
-# Add the port forwarding rule
-iptables -t nat -I PREROUTING 1 -i $INPUT_INTERFACE -p udp --dport 40000 -j DNAT --to $EDGE:40000
-iptables -t nat -I POSTROUTING 1 -o $INPUT_INTERFACE -p udp --source $EDGE --sport 40000 -j SNAT --to-source $INTERFACE_IP:40000
-
 # Ful Cone NAT
 #iptables -t nat -A POSTROUTING -o $INPUT_INTERFACE -j SNAT --to-source $IP_INTERFACE
 #iptables -t nat -A PREROUTING -i $INPUT_INTERFACE -p udp -j DNAT --to-destination $EDGE
+
+# Add the port forwarding rule
+iptables -t nat -I PREROUTING 1 -i $INPUT_INTERFACE -p udp --dport 33000 -j DNAT --to $EDGE:33000
+iptables -t nat -I POSTROUTING 1 -o $INPUT_INTERFACE -p udp --source $EDGE --sport 33000 -j SNAT --to-source $IP_INTERFACE:33000
+
+#iptables -t nat -A PREROUTING -i $INPUT_INTERFACE -p tcp --dport 12345 -j DNAT --to $EDGE:12345
+
