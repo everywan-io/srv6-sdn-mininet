e68226294715600dbfa5f07d22be4c3d9072f1d0
Carmine Scarpitta <carmine.scarpitta.94@gmail.com>
2020-02-08 20:18:54 +0100

    Remove all documents from the collections in MongoDB when the emulation is stopped

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,3 +6,6 @@
 killall turnserver
 # Stop the etherws virtual switch
 killall etherws
+# Remove documents from MongoDB
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.devices.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.overlays.remove({})"
