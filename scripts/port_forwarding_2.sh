9a104a5ed5b2d369ac8666e166ccfd9d11b06f2b
Carmine Scarpitta <carmine.scarpitta.94@gmail.com>
2020-02-17 00:41:36 +0100

    Add scripts

---
+++ scripts/port_forwarding_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script install a port forwarding on the NAT to allow incoming connections
+# to a given host on a given port
+
+# General imports
+source hostname.sh
+source nodes.sh
+source ips.sh
+source interfaces.sh
+
+# Name of the neighbor on the public net
+INPUT_INTERFACE=$1
+# Name of the host on the private net
+EDGE=$2
+# IP address of the host on the private net
+EDGE=${NODES[$EDGE]}
+# Name of the interface facing the public net
+INPUT_INTERFACE=${INTERFACES[$INPUT_INTERFACE]}
+# IP address of the interface facing the public net
+INTERFACE_IP=${IPS[$INPUT_INTERFACE]}
+
+# Add the port forwarding rule
+iptables -t nat -I PREROUTING 1 -i $INPUT_INTERFACE -p udp --dport 20000 -j DNAT --to $EDGE:4789
+iptables -t nat -I POSTROUTING 1 -o $INPUT_INTERFACE -p udp --source $EDGE --sport 4789 -j SNAT --to-source $INTERFACE_IP:20000
