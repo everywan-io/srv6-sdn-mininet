02460fbdc4034d1f49bd371b29526ee87a3e5e9f
Carmine Scarpitta <carmine.scarpitta.94@gmail.com>
2020-02-16 15:34:48 +0100

    Resolve merge conflicts

---
+++ scripts/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script is invoked on Mininet termination
+
+# Kill the turn server
+killall turnserver
+# Stop the etherws virtual switch
+killall etherws
+# Remove documents from MongoDB
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.devices.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.overlays.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.dev_to_ip.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.last_allocated_ip.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.last_allocated_vni.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.overlay_to_vni.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.reusable_ip.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.reusable_vni.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.slices_in_overlay.remove({})"
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.tenants.remove({})"
+#mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.tenants.insert({\"tenantid\": \"1\", \"conf\": {\"port\": NumberInt(4789)}})"  # TODO to be removed
+mongo "mongodb://root:12345678@localhost:27017/EveryWan?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" --eval "db.tenants.insert({\"tenantid\": \"1\"})"  # TODO to be removed
--- scripts/sdwan_controller.sh
+++ scripts/sdwan_controller.sh
@@ -31,6 +31,13 @@ echo $PATH
 #echo $NEIGHS
 echo $ips
 
+<<<<<<< Updated upstream
 #python -m srv6_sdn_control_plane.srv6_controller --ips $ips --period 10 --topology /tmp/topo.json --topo-graph /tmp/topo_graph.svg --sb-interface gRPC --nb-interface gRPC --grpc-server-ip :: --grpc-server-port 12345
 python -m srv6_sdn_control_plane.srv6_controller --ips $ips --period 10 --topology /tmp/topo.json --topo-graph /tmp/topo_graph.svg --sb-interface gRPC --nb-interface gRPC --grpc-server-ip :: --grpc-server-port 12345 --pymerang-server-ip ${NODES[$HOSTNAME]} --pymerang-server-port 50061
 #python -m nat_utils.nat_discovery_server
+=======
+# Start the etherws virtual switch
+etherws sw
+# Start the SD-WAN controller
+stdbuf -i0 -o0 -e0 python -m srv6_sdn_control_plane.srv6_controller --ips $ips --period 10 --topology /tmp/topo.json --topo-graph /tmp/topo_graph.svg --sb-interface gRPC --nb-interface gRPC --grpc-server-ip :: --grpc-server-port 54321 --pymerang-server-ip ${NODES[$HOSTNAME]} --pymerang-server-port 50061 &
+>>>>>>> Stashed changes
