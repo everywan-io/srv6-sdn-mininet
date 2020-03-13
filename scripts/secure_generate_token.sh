13e320f8770e181b4f9b4166720e6658e57e18b6
Carmine Scarpitta <carmine.scarpitta.94@gmail.com>
2020-03-13 01:13:34 +0100

    Add topologies and scripts to run a Mininet experiment with gRPC secure mode

---
+++ scripts/secure_generate_token.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script generates tenants for SD-WAN
+
+# General imports
+source nodes.sh
+source hostname.sh
+
+# Number of tokens to be generated
+N=$1
+# Get the IP address of the controller
+controller_ip=${NODES[$HOSTNAME]}
+# Generate the tenant configuration
+CONFIG='[{"tenantid": "1", "vxlan_port":40000, "tenant_info":"tenant01"}]'
+echo "$CONFIG" > /tmp/tenant_config.json
+# Wait for controller getting ready
+sleep 10
+# Generate N tokens
+for (( i=1; i<=N; i++ ))
+do
+  # Generate the token
+  token="$(python -m configure_tenant -s -c /tmp/ca.crt --controller-ip $controller_ip --controller-port 54321 --config-path /tmp/tenant_config.json)"
+  token=${token##*TOKEN:  }
+  echo "$token" > "/tmp/token-$i"
+done
---
+++ scripts/secure_sdwan_controller.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script implements the functionalities of a SD-WAN controller
+
+# General imports
+source nodes.sh
+source hostname.sh
+
+# Get the names of the hosts to which the controller must connect to
+# extract the topology
+nodes=( "$@" )
+
+# Build the mapping IP-port
+ips=""
+for node in ${nodes}
+do
+	ips+=${NODES[${node}]}-2606,
+done
+# Delete trailing comma
+ips="${ips:0:-1}"
+
+# Get the IP address of the controller
+controller_ip=${NODES[$HOSTNAME]}
+
+# Start the etherws virtual switch
+etherws sw
+# In our experiment, the controller is also our certification authority (CA)
+# Generate CA key
+openssl genrsa -out ca.key 2048
+# Generate CA certificate
+openssl req -new -x509 -days 365 -key ca.key -out ca.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com" 
+# Copy the CA certificate to /tmp, so it will be easy to find it for the edge devices
+cp ca.crt /tmp/ca.crt
+# Generate server key
+openssl genrsa -out server.key 2048
+# Generate server certificate signing request
+openssl req -new -key server.key -out server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:example.com,DNS:${NODES[$HOSTNAME]}"))
+# Generate server certificate
+openssl x509 -req -days 365 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out server.crt -extfile <(printf "basicConstraints = CA:FALSE\nkeyUsage = nonRepudiation, digitalSignature, keyEncipherment\nsubjectAltName = @alt_names\n[alt_names]\nDNS.1=example.com\nDNS.2=${NODES[$HOSTNAME]}")
+# Start the SD-WAN controller
+python -m srv6_sdn_control_plane.srv6_controller --ips $ips --period 10 --topology /tmp/topo.json --topo-graph /tmp/topo_graph.svg --sb-interface gRPC --nb-interface gRPC --grpc-server-ip :: --grpc-server-port 54321 --pymerang-server-ip ${NODES[$HOSTNAME]} --pymerang-server-port 50061 -s -x --sb-server-cert server.crt --sb-server-key server.key --nb-server-cert server.crt --nb-server-key server.key --client-cert /tmp/ca.crt &
