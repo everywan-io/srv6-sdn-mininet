44946bb2a4d29eb70cb36b69c9b4b3cd6f8c6bd4
bogdaniatco <bogdan.iatco@hotmail.it>
2020-04-19 11:29:34 +0200

    Network topo and script to bootstrap ewED4 for video demo

---
+++ EveryEdge.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script implements the functionalities of an EveryWAN Edge Device
+
+# General imports
+source devid.sh
+source hostname.sh
+source nodes.sh
+source ips.sh
+source interfaces.sh
+
+# Contoller hostname
+CONTROLLER="ewCont"
+# NAT discovery server hostname
+NAT="natDS"
+# Neighbor through which the NAT discovery request has to be sent
+NAT_DISCOVERY_CLIENT="modem4"
+
+# Get the IP address of the controller
+CONTROLLER=${NODES[$CONTROLLER]}
+# Get the IP address of the NAT discovery server
+NAT=${NODES[$NAT]}
+# Get the IP address of the output interface used by the NAT discovery procedure
+NAT_DISCOVERY_CLIENT=${IPS[${INTERFACES[$NAT_DISCOVERY_CLIENT]}]}
+
+# Generate the device configuration
+CONFIG='{"id":"'$DEVICEID'","features":[{"name":"gRPC","port":12345},{"name":"SSH","port":22}]}'
+# Export the configuration
+jq -n ${CONFIG} > /tmp/config-${HOSTNAME}.json
+
+# Start etherws virtual switch
+etherws sw
+# Wait for the controller getting ready
+#sleep 15
+# Start the southbound gRPC server
+#python -m srv6_sdn_data_plane.southbound.grpc.sb_grpc_server --debug &
+# Wait
+#sleep 5
+# Start the registration client
+#python -m pymerang.pymerang_client --config-file /tmp/config-${HOSTNAME}.json --nat-discovery-server-ip $NAT --nat-discovery-client-ip $NAT_DISCOVERY_CLIENT --server-ip $CONTROLLER --server-port 50061 --token-file ./token &
+python -m srv6_sdn_data_plane.ew_edge_device --device-config-file /tmp/config-${HOSTNAME}.json --nat-discovery-server-ip $NAT --nat-discovery-client-ip $NAT_DISCOVERY_CLIENT --pymerang-server-ip $CONTROLLER --pymerang-server-port 50061 --token-file ./token &
