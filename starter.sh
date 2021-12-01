29a0578557373a8dd24eae478a22971ceecaec25
Carmine Scarpitta <carmine.scarpitta@uniroma2.it>
2021-12-01 01:36:07 +0100

    Add IPv6 topology

--- starter.sh
+++ starter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source /home/user/Envs/srv6env/bin/activate
-cd /home/user/repos/srv6-sdn-mininet
-sudo /home/user/Envs/srv6env/bin/python ./srv6_mininet_extension.py --topo topo/topology_h_multisub.json --debug --ipv4
+source /home/rose/workspace/everywan/.everywan-venv/bin/activate
+cd /home/rose/workspace/everywan/srv6-sdn-mininet
+#sudo /home/rose/workspace/everywan/.everywan-venv/bin/python ./srv6_mininet_extension.py --topo topo/topology_h_multisub.json --debug --ipv4
+sudo /home/rose/workspace/everywan/.everywan-venv/bin/python ./srv6_mininet_extension.py --topo topo/topology_h_multisub_ipv6.json --debug
 #sudo /home/user/Envs/srv6env/bin/python ./srv6_mininet_extension.py --topo topo/example_srv6_topology_small.json --debug
